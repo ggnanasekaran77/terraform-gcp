57da7117637f80e95a540310b5a3ec561f1c1b54
= <=>
2022-05-21 07:05:21 +0800

    terraform output variable

--- main.tf
+++ main.tf
@@ -15,3 +15,23 @@ terraform {
     credentials = "gcp-tf-course.json"
   } 
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {   
+    }
+  } 
+}
+resource "google_compute_address" "static_ip" {
+  name = "terraform-static-ip"
+}
